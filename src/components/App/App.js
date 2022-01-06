0f15b3f11bfeb9512caf1690f503d3575cb4b777
Haojun Yuan <brianyuan7@gmail.com>
2022-01-06 03:01:50 -0500

    foodie

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+import BusinessList from '../BusinessList/BusinessList';
+import { render } from '@testing-library/react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>foodie</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
+}
+
+export default App;
